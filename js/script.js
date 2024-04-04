3270b9eb1a1ccd5a89e4d2a3a9c9603ec69ccb3a
JulioFontes06 <julio.developer06@gmail.com>
2024-04-04 13:15:58 -0300

    Auteração no script.js

--- js/script.js
+++ js/script.js
@@ -1,267 +1,174 @@
+/* 
+
+const submitBtn = document.getElementById("submit");
+*/
+
+let abstractPlan = {
+  todo: [],
+  doing: [],
+  done: [],
+};
+
+/* Adicionar task */
+
+const addNewTask = () => {
   const ipt = document.getElementById("toDoTaskCadaster");
+  const toDo = document.getElementById("toDo");
+  const task = document.querySelectorAll(".task");
+
+  task.forEach((item) => {
+    item?.remove("li");
+  });
+
+  if (ipt.value) {
+    abstractPlan.todo.unshift(ipt.value);
+    console.clear();
+    console.log(abstractPlan);
+    display(ipt.value);
+  }
+};
+
+/* Mostrar na tela */
+
+const showOnScreen = () => {
   const toDo = document.getElementById("toDo");
   const doing = document.getElementById("doing");
   const done = document.getElementById("done");
-  const submutBtn = document.getElementById('submit')
-
-  let abstractPlan = {
-    todo: [],
-    doing: [],
-    done: [],
-  };
-
-  let toDoTaskNumber =
-    abstractPlan.todo.length +
-    abstractPlan.doing.length +
-    abstractPlan.done.length;
-
-  startToDoList();
-
-
-  function addTask() {
-    if (ipt.value) {
-      createLi(ipt.value);
-      ipt.value = "";
-    }
-  }
-
-  submutBtn.addEventListener('click', () => {
-    addTask()
+  updateLocalStorage();
+  let localStorageDatabase = localStorage.getItem("dataBase");
+  localStorageDatabase = JSON.parse(localStorageDatabase);
+  localStorageDatabase.todo.forEach((task) => {
+    newLi(toDo, task, 'todo');
+  });
+  localStorageDatabase.doing.forEach((task) => {
+    newLi(doing, task, 'doing')
   })
+  localStorageDatabase.done.forEach((task) => {
+    newLi(done, task, 'done')
+  })
+};
 
-  function createLi(content) {
-    const li = document.createElement("li");
-    const h1 = document.createElement("h1");
-    const img = document.createElement("img");
+const display = (task) => {
+  newLi(toDo, task, 'todo');
+}
 
-    li.id = `tsk-${toDoTaskNumber}-li`;
-    img.id = `tsk-${toDoTaskNumber}-img`;
-    li.className = "task";
+/* Criar a LI */
 
-    h1.innerText = content;
-    img.src = "./src/trash3-fill.svg";
-    img.className = "trash";
+const newLi = (data, content, localState) => {
+  const li = document.createElement("li");
+  const img = document.createElement("img");
 
-    abstractPlan.todo.unshift(content);
-    li.append(h1, img);
-    toDo.append(li);
-    toDoTaskNumber++;
+  li.className = `task-${localState}`;
+  li.innerText = content;
+  img.src = "./src/trash3-fill.svg";
+  img.addEventListener("click", (e) => remove(e));
+  li.addEventListener('click', (e) => toggle(e))
 
-    img.addEventListener("click", (e) => {
-      const word = e.target.parentNode.firstChild;
-      excludePlan(word, e);
-      console.log(word.innerText);
-      e.target.parentNode.remove();
-      const currentAbstractPlanString = JSON.stringify(abstractPlan);
-      localStorage.setItem("abstractPlan", currentAbstractPlanString);
-    });
+  li.append(img);
 
-    const abstractPlanString = JSON.stringify(abstractPlan);
-    localStorage.setItem("abstractPlan", abstractPlanString);
-    li.addEventListener("click", (e) => {
-      const task = e.target.parentNode;
+  data.append(li);
+};
 
-      if (e?.target?.parentNode?.parentNode?.id) {
-        if (e.target.parentNode.parentNode.id === "toDo") {
-          toDoForDoing(e.target.innerText);
-          task.remove();
-          doing.appendChild(task);
-          // console.table(abstractPlan)
-          const currentAbstractPlanString = JSON.stringify(abstractPlan);
-          localStorage.setItem("abstractPlan", currentAbstractPlanString);
-        } else if (e.target.parentNode.parentNode.id === "doing") {
-          doingForDone(e.target.innerText);
-          task.remove();
-          done.appendChild(task);
-          // console.table(abstractPlan)
-          const currentAbstractPlanString = JSON.stringify(abstractPlan);
-          localStorage.setItem("abstractPlan", currentAbstractPlanString);
-        } else if (e.target.parentNode.parentNode.id === "done") {
-          doneForToDo(e.target.innerText);
-          task.remove();
-          toDo.appendChild(task);
-          // console.table(abstractPlan)
-          const currentAbstractPlanString = JSON.stringify(abstractPlan);
-          localStorage.setItem("abstractPlan", currentAbstractPlanString);
-        }
-      }
-    });
+/* Remover a li */
+
+const remove = (e) => {
+  e.target.parentElement.remove();
+
+  let indexToDo = abstractPlan.todo.findIndex(
+    (i) => i === e.target.parentElement.innerText
+  );
+
+  console.log(indexToDo);
+  if (indexToDo !== -1) {
+    abstractPlan.todo.splice(indexToDo, 1);
   }
 
-  function excludePlan(word) {
-    const indexTodo = abstractPlan.todo.indexOf(word.innerText);
-    if (indexTodo !== -1) {
-      abstractPlan.todo.splice(indexTodo, 1);
+  let indexDoing = abstractPlan.doing.findIndex(
+    (i) => i === e.target.parentElement.innerText
+  );
+
+  console.log(indexDoing);
+  if(indexDoing !== -1) {
+    abstractPlan.doing.splice(indexDoing, 1)
+  }
+
+  let indexDone = abstractPlan.done.findIndex(
+    (i) => i === e.target.parentElement.innerText
+  );
+
+  console.log(indexDone);
+  if(indexDone !== -1) {
+    abstractPlan.done.splice(indexDone, 1)
+  }
+
+  updateLocalStorage();
+};
+
+const start = () => {
+  let localStorageData = localStorage.getItem("dataBase");
+  localStorageData = JSON.parse(localStorageData);
+
+  if (
+    localStorageData.todo ||
+    localStorageData.doing ||
+    localStorageData.done
+  ) {
+    abstractPlan = localStorageData;
+  }
+};
+
+const updateLocalStorage = () => {
+  let localStorageData = JSON.stringify(abstractPlan);
+  console.log(localStorageData);
+
+  localStorage.setItem("dataBase", localStorageData);
+};
+
+const toggle = (e) => {
+    const toDo = document.getElementById('toDo')
+    const doing = document.getElementById('doing')
+    const done = document.getElementById('done')
+    let event = e.target
+
+    const indexToDo = abstractPlan.todo.findIndex(i => i === event.innerText)
+
+    if(event.className === 'task-todo' && indexToDo !== -1) {
+        abstractPlan.todo.splice(indexToDo, 1)
+        abstractPlan.doing.unshift(event.innerText)
+        event.classList = 'task-doing' 
+        event.remove()
+        doing.appendChild(event)
+        updateLocalStorage()
+        return
     }
 
-    const indexDoing = abstractPlan.doing.indexOf(word.innerText);
-    if (indexDoing !== -1) {
-      abstractPlan.doing.splice(indexDoing, 1);
+    const indexDoing = abstractPlan.doing.findIndex(i => i === event.innerText)
+    
+    if(event.className === 'task-doing' && indexDoing !== -1) {
+        abstractPlan.doing.splice(indexDoing, 1)
+        abstractPlan.done.unshift(event.innerText)
+        event.classList = 'task-done'
+        event.remove()
+        done.appendChild(event)
+        updateLocalStorage()
+        return
     }
 
-    const indexDone = abstractPlan.done.indexOf(word.innerText);
-    if (indexDone !== -1) {
-      abstractPlan.done.splice(indexDone, 1);
+    const indexDone = abstractPlan.done.findIndex(i => i === event.innerText)
+
+    if(event.className === 'task-done' && indexDone !== -1) {
+        abstractPlan.done.splice(indexDone, 1)
+        abstractPlan.todo.unshift(event.innerText)
+        event.classList = 'task-todo'
+        event.remove()
+        toDo.appendChild(event)
+        updateLocalStorage()
+        return
     }
-  }
+}
 
-  // bug para corrigir no abstractPlan
 
-  function startToDoList() {
-    let abstractPlanLocal = localStorage.getItem("abstractPlan");
-    abstractPlanLocalStorage = JSON.parse(abstractPlanLocal);
-    abstractPlan = abstractPlanLocalStorage;
-    console.log(abstractPlanLocal);
 
-    if (abstractPlan) {
-      // const currentAbstractPlan = JSON.stringify(abstractPlan)
-      // localStorage.setItem("abstractPlan", currentAbstractPlan)
-      abstractPlan.todo.forEach((task, i) => {
-        const li = document.createElement("li");
-        const h1 = document.createElement("h1");
-        const img = document.createElement("img");
-
-        li.id = `tsk-${i}-li`;
-        img.id = `tsk-${i}-img`;
-        li.className = "task";
-
-        h1.innerText = task;
-        img.src = "./src/trash3-fill.svg";
-        img.className = "trash";
-
-        toDo.appendChild(li);
-        li.append(h1, img);
-
-        const abstractPlanString = JSON.stringify(abstractPlan);
-        localStorage.setItem("abstractPlan", abstractPlanString);
-        li.addEventListener("click", (e) => {
-          const task = e.target.parentNode;
-
-          if (e?.target?.parentNode?.parentNode?.id) {
-            if (e.target.parentNode.parentNode.id === "toDo") {
-              toDoForDoing(e.target.innerText);
-              task.remove();
-              doing.appendChild(task);
-              // console.table(abstractPlan)
-              const currentAbstractPlanString = JSON.stringify(abstractPlan);
-              localStorage.setItem("abstractPlan", currentAbstractPlanString);
-            }
-          }
-        });
-
-        img.addEventListener("click", (e) => {
-          const word = e.target.parentNode.firstChild;
-          excludePlan(word);
-          console.log(word.innerText);
-          e.target.parentNode.remove();
-          const currentAbstractPlanString = JSON.stringify(abstractPlan);
-          localStorage.setItem("abstractPlan", currentAbstractPlanString);
-        });
-      });
-
-      abstractPlan.doing.forEach((task, i) => {
-        const li = document.createElement("li");
-        const h1 = document.createElement("h1");
-        const img = document.createElement("img");
-
-        li.id = `tsk-${i}-li`;
-        img.id = `tsk-${i}-img`;
-        li.className = "task";
-
-        h1.innerText = task;
-        img.src = "./src/trash3-fill.svg";
-        img.className = "trash";
-
-        doing.appendChild(li);
-        li.append(h1, img);
-
-        const abstractPlanString = JSON.stringify(abstractPlan);
-        localStorage.setItem("abstractPlan", abstractPlanString);
-        li.addEventListener("click", (e) => {
-          const task = e.target.parentNode;
-
-          if (e?.target?.parentNode?.parentNode?.id) {
-            if (e.target.parentNode.parentNode.id === "doing") {
-              doingForDone(e.target.innerText);
-              task.remove();
-              done.appendChild(task);
-              // console.table(abstractPlan)
-              const currentAbstractPlanString = JSON.stringify(abstractPlan);
-              localStorage.setItem("abstractPlan", currentAbstractPlanString);
-            }
-          }
-        });
-
-        img.addEventListener("click", (e) => {
-          const word = e.target.parentNode.firstChild;
-          excludePlan(word);
-          console.log(word.innerText);
-          e.target.parentNode.remove();
-          const currentAbstractPlanString = JSON.stringify(abstractPlan);
-          localStorage.setItem("abstractPlan", currentAbstractPlanString);
-        });
-      });
-
-      abstractPlan.done.forEach((task, i) => {
-        const li = document.createElement("li");
-        const h1 = document.createElement("h1");
-        const img = document.createElement("img");
-
-        li.id = `tsk-${i}-li`;
-        img.id = `tsk-${i}-img`;
-        li.className = "task";
-
-        h1.innerText = task;
-        img.src = "./src/trash3-fill.svg";
-        img.className = "trash";
-
-        done.appendChild(li);
-        li.append(h1, img);
-
-        const abstractPlanString = JSON.stringify(abstractPlan);
-        localStorage.setItem("abstractPlan", abstractPlanString);
-        li.addEventListener("click", (e) => {
-          const task = e.target.parentNode;
-
-          if (e?.target?.parentNode?.parentNode?.id) {
-            if (e.target.parentNode.parentNode.id === "done") {
-              doneForToDo(e.target.innerText);
-              task.remove();
-              toDo.appendChild(task);
-              // console.table(abstractPlan)
-              const currentAbstractPlanString = JSON.stringify(abstractPlan);
-              localStorage.setItem("abstractPlan", currentAbstractPlanString);
-            }
-          }
-        });
-
-        img.addEventListener("click", (e) => {
-          const word = e.target.parentNode.firstChild;
-          excludePlan(word);
-          console.log(word.innerText);
-          e.target.parentNode.remove();
-          const currentAbstractPlanString = JSON.stringify(abstractPlan);
-          localStorage.setItem("abstractPlan", currentAbstractPlanString);
-        });
-      });
-    }
-  }
-
-  function toDoForDoing(value) {
-    const index = abstractPlan.todo.findIndex((i) => i === value);
-    abstractPlan.todo.splice(index, 1);
-    abstractPlan.doing.push(value);
-  }
-
-  function doingForDone(value) {
-    const index = abstractPlan.doing.findIndex((i) => i === value);
-    abstractPlan.doing.splice(index, 1);
-    abstractPlan.done.push(value);
-  }
-
-  function doneForToDo(value) {
-    const index = abstractPlan.done.findIndex((i) => i === value);
-    abstractPlan.done.splice(index, 1);
-    abstractPlan.todo.push(value);
-  }
+start();
+showOnScreen();
 
